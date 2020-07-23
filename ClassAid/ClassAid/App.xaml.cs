1848a430950f7f1978a168d9a1f5cfa5587e6a8c
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-07-24 00:41:06 +0600

    Created Demo

--- ClassAid/ClassAid/App.xaml.cs
+++ ClassAid/ClassAid/App.xaml.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace ClassAid
 {
     public partial class App : Application
     {
+        public static string FolderPath { get; private set; }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // MainPage = new MainPage();
+            InitializeComponent();
+            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new NotesPage());
         }
 
         protected override void OnStart()
