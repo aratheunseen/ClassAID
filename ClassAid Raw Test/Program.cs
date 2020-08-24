00bd73436cfe01b7865257077ac2bfd5e9eed311
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-08-24 14:42:05 +0600

    Finsihed creating admin regestration api

---
+++ ClassAid Raw Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ClassAidUniversal.WebConnection;
+using ClassAidUniversal.Users;
+namespace ClassAid_Raw_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Admin admin = new Admin();
+            admin.UserName = "HasanXX";
+            ///"UserName": "MahmudXX",
+            admin.Password = "iamawesome";
+            admin.Email = "ggg@hello.com";
+            admin.Phone = "+8801915577428";
+            admin.AdminKey = admin.GetID();
+            
+
+
+            string res = RegisterAdmin.Register(admin);
+            Console.WriteLine(res);
+        }
+    }
+}
