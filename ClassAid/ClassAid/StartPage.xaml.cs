832caa851c40b685e960d7068f38d181528e8768
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-21 00:09:37 +0600

    Applied MVC pricipals and added a bypass btn.

--- ClassAid/ClassAid/StartPage.xaml.cs
+++ ClassAid/ClassAid/StartPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ClassAid.Views;
+﻿using ClassAid.Models.Users;
+using ClassAid.Views.AdminViews;
+using ClassAid.Views.StudentViews;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +38,18 @@ namespace ClassAid
         private void SwipeGestureRecognizer_Swiped_1(object sender, SwipedEventArgs e)
         {
             swapText.Text = "Swaped right";
+        }
+
+        private void bypassBtn_Clicked(object sender, EventArgs e)
+        {
+            Admin admin = new Admin("HolaSenorita","IamAwesome");
+            admin.Email = "mail@iloveyou.com";
+            admin.ID = "192311000";
+            admin.Name = "Jon Doe";
+            admin.Phone = 01911104587;
+            Application.Current.MainPage = new NavigationPage(new AboutPage(admin));
+
+
         }
     }
 }
