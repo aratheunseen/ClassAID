a899e738fe3ef747c88fcec63a46e232858005c2
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-24 00:13:43 +0600

    added azure app center dependency

--- ClassAid/ClassAid/App.xaml.cs
+++ ClassAid/ClassAid/App.xaml.cs
@@ -2,7 +2,9 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ClassAid.Views;
-
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 namespace ClassAid
 {
     public partial class App : Application
@@ -16,7 +18,10 @@ namespace ClassAid
 
         protected override void OnStart()
         {
-
+            AppCenter.Start("android=6c9e3d95-2ad6-4960-bdd8-d63483b3a7a2;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
