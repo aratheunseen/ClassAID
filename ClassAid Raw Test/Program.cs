59d4c9085c9f31423f25853a619dd21464af29f1
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-24 00:13:43 +0600

    added azure app center dependency

--- ClassAid Raw Test/Program.cs
+++ ClassAid Raw Test/Program.cs
@@ -1,33 +1,9 @@
-﻿using System;
-using ClassAidUniversal.Users;
-using System.Linq;
-
-namespace ClassAid_Raw_Test
+﻿namespace ClassAid_Raw_Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Admin admin = new Admin("HeloSenorita", "xoxoIamAwesome");
-            admin.Username = "HeloSenorita";
-            admin.Password = "xoxoIamAwesome";
-            Console.WriteLine(admin.Key);
-            //Student s = new Student();
-            //s.ID = "192311030";
-            //s.Name = "Jack Sparrow";
-            //List<Student> sl = new List<Student>();
-            //sl.Add(s);
-            //sl.Add(s);
-            //sl.Add(s);
-            //sl.Add(s);
-            Console.WriteLine("Hello");
-            //Yo Wassup
-            //admin.UserBase = Newtonsoft.Json.JsonConvert.SerializeObject(sl);
-            Console.WriteLine(admin.StudentList.Count());
-            //admin.UserBase = Newtonsoft.Json.JsonConvert.SerializeObject(sl);
-            Console.WriteLine(admin.StudentList.Count());
-            //string res = RegisterAdmin.Register(admin);
-            //Console.WriteLine(res);
         }
     }
 }
