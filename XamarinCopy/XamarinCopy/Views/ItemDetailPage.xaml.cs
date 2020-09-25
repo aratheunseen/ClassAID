acb1ae8ee829a3502388b96df6c4564a7ca6f19e
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-25 20:34:42 +0600

    fixed btn problem

---
+++ XamarinCopy/XamarinCopy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinCopy.ViewModels;
+
+namespace XamarinCopy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
