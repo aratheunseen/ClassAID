aae7591bbe29dd711b988315ea89a5f3f6c6e553
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-26 14:49:59 +0600

    Created tapable login button

--- ClassAid/ClassAid/Views/StudentViews/StudentLoginPage.xaml.cs
+++ ClassAid/ClassAid/Views/StudentViews/StudentLoginPage.xaml.cs
@@ -12,9 +12,12 @@ namespace ClassAid.Views.StudentViews
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StudentLoginPage : ContentPage
     {
+        public static Command TapCommand;
         public StudentLoginPage()
         {
             InitializeComponent();
+            TapCommand = new Command(async () => await Navigation.PushAsync(new StudentRegestrationPage()));
+            gst.Command = TapCommand;
         }
     }
 }
