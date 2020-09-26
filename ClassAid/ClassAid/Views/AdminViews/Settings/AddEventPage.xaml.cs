fb98d8d35e51045eac45f3149ca783a143601597
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-26 21:10:27 +0600

    Added event add page to create events. [Not implimented properly]

---
+++ ClassAid/ClassAid/Views/AdminViews/Settings/AddEventPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TEditor;
+using TEditor.Abstractions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClassAid.Views.AdminViews.Settings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddEventPage : ContentPage
+    {
+        public AddEventPage()
+        {
+            InitializeComponent();            
+        }        
+
+        private async void t_Clicked(object sender, EventArgs e)
+        {
+            var toolbar = new ToolbarBuilder().AddBasic().AddH1();
+            TEditorResponse response = await CrossTEditor.Current.ShowTEditor(null, toolbar);
+            if (!string.IsNullOrEmpty(response.HTML))
+                displayWebView.Source = new HtmlWebViewSource() { Html = response.HTML };
+        }
+    }
+}
