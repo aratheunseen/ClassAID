ff73e650c57941526e5b569a76149c273bbfb6f2
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-26 15:15:09 +0600

    Made login text clickable for admin

---
+++ ClassAid/ClassAid/Views/AdminViews/AdminLoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClassAid.Views.AdminViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdminLoginPage : ContentPage
+    {
+        public AdminLoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
