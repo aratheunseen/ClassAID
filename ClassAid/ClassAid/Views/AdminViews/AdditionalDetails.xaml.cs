d8b5c21f4a4ad45b72f354017942a9dd7e382831
Ashiqur Rahman Alif <62181222+aratheunseen@users.noreply.github.com>
2020-09-27 04:25:36 +0600

    AdditionalDetailsPage Design

--- ClassAid/ClassAid/Views/AdminViews/AdditionalDetails.xaml.cs
+++ ClassAid/ClassAid/Views/AdminViews/AdditionalDetails.xaml.cs
@@ -23,7 +23,7 @@ namespace ClassAid.Views.AdminViews
         private async void completeSignUp_Clicked(object sender, EventArgs e)
         {
             admin.Name = userRealName.Text;
-            admin.Email = userEmail.Text;
+        //    admin.Email = userEmail.Text;
             admin.Phone = long.Parse(userPhone.Text);
             try
             {
