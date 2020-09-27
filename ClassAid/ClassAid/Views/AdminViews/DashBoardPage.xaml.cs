abe85054087961098bea54083b0402ed34fcde61
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-27 07:50:07 +0600

    Marked Local db as json

--- ClassAid/ClassAid/Views/AdminViews/DashBoardPage.xaml.cs
+++ ClassAid/ClassAid/Views/AdminViews/DashBoardPage.xaml.cs
@@ -8,6 +8,7 @@ using ClassAid.Views.AdminViews.Settings;
 using Firebase.Database;
 using ClassAid.DataContex;
 using ClassAid.Models.Engines;
+using System.Threading.Tasks;
 
 namespace ClassAid.Views.AdminViews
 {
@@ -17,27 +18,14 @@ namespace ClassAid.Views.AdminViews
         public static ObservableCollection<ScheduleModel> scheduleCores;
         public static ObservableCollection<Teacher> teachers;
         public static ObservableCollection<EventModel> eventModels;
-        public static LocalAdminStorageContex LocalStorage;
-        public static LocalAdminStorageContex Database
-        {
-            get
-            {
-                if (LocalStorage == null)
-                {
-                    LocalStorage = new LocalAdminStorageContex();
-                }
-                return LocalStorage;
-            }
-        }
         FirebaseClient client;
         private bool canExit = false;
         public Admin admin;
         public string RealName { get { return admin.Name; } }
         public DashBoardPage(LoginAuthModel authModel)
         {
-            Action action = async () => admin = await Database.GetItemsAsync(authModel.key);
-            action.Invoke();
             InitializeComponent();
+            admin = authModel.AdminData;
             LoadVariables();
         }
         public DashBoardPage(Admin admin)
@@ -61,7 +49,7 @@ namespace ClassAid.Views.AdminViews
             eventsListView.ItemsSource = admin.EventList;
         }
         protected override bool OnBackButtonPressed()
-        {            
+        {
             if (!canExit)
             {
                 DependencyService.Get<Toast>().Show("Press again to Exit.");
@@ -75,12 +63,12 @@ namespace ClassAid.Views.AdminViews
 
         private void scheduleRefreshView_Refreshing(object sender, EventArgs e)
         {
-           scheduleRefreshView.IsRefreshing = false;
+            scheduleRefreshView.IsRefreshing = false;
         }
 
         private void teacherAddBtn_Clicked(object sender, EventArgs e)
         {
-            Navigation.PushAsync(new AddTeacherPage(admin,client));
+            Navigation.PushAsync(new AddTeacherPage(admin, client));
         }
 
         private void scheduleAddBtn_Clicked(object sender, EventArgs e)
