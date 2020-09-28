2575abd8bf91cbc43a5632cb782f1655d3504441
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-29 04:10:48 +0600

    Created custom db contex

--- ClassAid/ClassAid.Android/MainActivity.cs
+++ ClassAid/ClassAid.Android/MainActivity.cs
@@ -22,6 +22,7 @@ namespace ClassAid.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             //Xamarin.Essentials.Platform.Init(this, bundle);
+           
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
