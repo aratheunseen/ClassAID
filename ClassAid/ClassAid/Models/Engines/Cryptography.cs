e0245544ef3490c76dd55c618b503febaa3d3227
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-30 05:17:52 +0600

    Finished Admin Login and regestration pipeline with firebase.

--- ClassAid/ClassAid/Models/Engines/Cryptography.cs
+++ ClassAid/ClassAid/Models/Engines/Cryptography.cs
@@ -22,13 +22,13 @@ namespace ClassAid.Models.Engines
                 {
                     ICryptoTransform cryptoTransform = tripleDES.CreateEncryptor();
                     byte[] result = cryptoTransform.TransformFinalBlock(data, 0, data.Length);
-                    return Convert.ToBase64String(result, 0, result.Length);
+                    return Convert.ToBase64String(result, 0, result.Length).Replace("/","SLASHAWESOME");
                 }
             }
         }
         public static string DecryptString(string text, string key)
         {
-            byte[] data = Convert.FromBase64String(text);
+            byte[] data = Convert.FromBase64String(text.Replace("SLASHAWESOME", "/"));
             using (MD5CryptoServiceProvider mD5 = new MD5CryptoServiceProvider())
             {
                 byte[] keys = mD5.ComputeHash(Encoding.UTF8.GetBytes(key));
--- ClassAid/ClassAid/Models/Users/Student.cs
+++ ClassAid/ClassAid/Models/Users/Student.cs
@@ -4,26 +4,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ClassAid.Models.Users
 {
-    public class Student
+    public class Student : Shared
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
         public Student(string Username, string Password)
         {
             this.Username = Username;
             this.Password = Password;
             Key = Cryptography.EncryptString(this.Username, this.Password);
         }
-        public string Key { get; private set; }
-        [Required]
+        public Student()
+        {
+
+        }
         public string AdminKey { get; set; }
-        public string Name { get; set; }
-        [EmailAddress]
-        public string Email { get; set; }
-        [Phone]
-        public string Phone { get; set; }
-        public string ID { get; set; }
-        public DateTime JoinDate { get { return DateTime.Now; } }
         public bool IsActive { get; set; }
 
     }
