a57c7ecd69dc6a47b6d1edb2d5cffc95ef0cbf37
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-01 04:16:26 +0600

    Fixed "Sharing violation IOException"

--- ClassAid/ClassAid/DataContex/FirebaseHandler.cs
+++ ClassAid/ClassAid/DataContex/FirebaseHandler.cs
@@ -1,10 +1,12 @@
-﻿using ClassAid.Models.Users;
+﻿using ClassAid.Models;
+using ClassAid.Models.Users;
 using Firebase.Database;
 using Firebase.Database.Query;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace ClassAid.DataContex
 {
@@ -26,19 +28,25 @@ namespace ClassAid.DataContex
         }
         public static async Task InsertData(Shared user)
         {
+            LocalStorageEngine.SaveDataAsync(user, FileType.Shared);
             await client
                 .Child(TableName(user.IsAdmin))
                 .Child(user.Key)
                 .PostAsync(user);
-            LocalStorageEngine.SaveDataAsync(user, FileType.Admin);
         }
         public static async Task UpdateAdmin(Shared user)
         {
-            await client
-              .Child(TableName(user.IsAdmin))
-              .Child(user.Key)
-              .PutAsync(user);
-            LocalStorageEngine.SaveDataAsync(user, FileType.Admin);
+            LocalStorageEngine.SaveDataAsync(user, FileType.Shared);
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+            {
+                await client
+              .Child(TableName(user.IsAdmin)).Child(user.Key).PutAsync(user);
+            }
+            else
+            {
+                DependencyService.Get<Toast>().Show("No Internet connection. Saved for later syncing.");
+                Preferences.Set(PrefKeys.isSyncPending, true);
+            }
         }
         #region RealTime
         //public static async Task<T> RealTimeConnection<T>(string tablename, T data)
@@ -65,7 +73,7 @@ namespace ClassAid.DataContex
               .Child(TableName(IsAdmin))
               .OnceAsync<Shared>()).Select(item => item.Object)
             .Where(item => item.Key == key).FirstOrDefault();
-            LocalStorageEngine.SaveDataAsync(res, FileType.Admin);
+            LocalStorageEngine.SaveDataAsync(res, FileType.Shared);
             return res;
         }
         private static string TableName(bool IsAdmin)
