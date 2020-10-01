9b20bf2a3d1d0df2c6519e64a3b6d5553ae1f49a
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-01 17:55:09 +0600

    Added custom picker.

---
+++ ClassAid/ClassAid.Android/ComponentsRenderer/PickerRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Android.Content;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using ClassAid.CustomComponents;
+using ClassAid.Droid.ComponentsRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ClassAidPicker), typeof(CustomPickerRenderer))]
+namespace ClassAid.Droid.ComponentsRenderer
+{
+    class CustomPickerRenderer : PickerRenderer
+    {
+        [System.Obsolete]
+        public CustomPickerRenderer(Context context) : base(context)
+        {
+
+        }
+
+        [System.Obsolete]
+#pragma warning disable CS0809 
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+#pragma warning restore CS0809 
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Android.Graphics.Color placeHolderColor = new Android.Graphics.Color(200, 198, 196);
+                GradientDrawable gd = new GradientDrawable();
+                gd.SetColor(Android.Graphics.Color.Transparent);
+                Control.SetBackgroundDrawable(gd);
+                Control.SetHintTextColor(ColorStateList.ValueOf(placeHolderColor));
+            }
+        }
+    }
+}
