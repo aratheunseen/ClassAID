c4a9e838f91d1986a9cba729e1bf4af1a9108d43
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-03 17:27:59 +0600

    Added Firebase storage for file sharing

---
+++ ClassAid/ClassAid/DataContex/FirebaseStorageHandler.cs
@@ -0,0 +1,30 @@
+﻿using Firebase.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAid.DataContex
+{
+    class FirebaseStorageHandler
+    {
+        public async static Task<string> SaveFile(string path)
+        {
+            var stream = File.Open(path, FileMode.Open);
+
+            // Constructr FirebaseStorage, path to where you want to upload the file and Put it there
+            var task = new FirebaseStorage("gs://classaidapp.appspot.com")
+                .Child("data")
+                .PutAsync(stream);
+
+            ////// Track progress of the upload
+            ////task.Progress.ProgressChanged += (s, e) => 
+            ////Console.WriteLine($"Progress: {e.Percentage} %");
+
+            // await the task to wait until upload completes and get the download url
+            var downloadUrl = await task;
+            return downloadUrl;
+        }
+    }
+}
