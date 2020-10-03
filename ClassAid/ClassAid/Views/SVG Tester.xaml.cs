1f754b6969a6674ec0369756f44ca5948adcea5e
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-03 17:27:59 +0600

    Added Firebase storage for file sharing

--- ClassAid/ClassAid/Views/SVG Tester.xaml.cs
+++ ClassAid/ClassAid/Views/SVG Tester.xaml.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Android.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.FilePicker;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ClassAid.DataContex;
 
 namespace ClassAid.Views
 {
@@ -15,6 +18,19 @@ namespace ClassAid.Views
         public SVG_Tester()
         {
             InitializeComponent();
+            Intent intent = new Intent();
+            
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var file = await CrossFilePicker.Current.PickFile();
+
+            if (file != null)
+            {                
+                string url = await FirebaseStorageHandler.SaveFile(file.FilePath);
+                lbl.Text = url;
+            }
         }
     }
 }
