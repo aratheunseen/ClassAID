4136139ffc4af6abdeef4e2c24ae853210903b3a
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-04 11:14:31 +0600

    Tested realtime and it works (almost)

--- ClassAid/ClassAid/Views/SVG Tester.xaml.cs
+++ ClassAid/ClassAid/Views/SVG Tester.xaml.cs
@@ -4,6 +4,8 @@ using Plugin.FilePicker;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ClassAid.DataContex;
+using Firebase.Storage;
+using System.Collections.ObjectModel;
 
 namespace ClassAid.Views
 {
@@ -13,7 +15,11 @@ namespace ClassAid.Views
         public SVG_Tester()
         {
             InitializeComponent();
-            
+            ObservableCollection<string> coll = new ObservableCollection<string>();
+            coll.Add("Hello!");
+            Action ac = (async()=> await FirebaseHandler.ListOfAdmin(coll));
+            ac.Invoke();
+            adminCodeList.ItemsSource = coll;
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -21,10 +27,25 @@ namespace ClassAid.Views
             var file = await CrossFilePicker.Current.PickFile();
 
             if (file != null)
-            {                
-                string url = await FirebaseStorageHandler.SaveFile(file.FilePath);
+            {
+                //string url = await FirebaseStorageHandler.SaveFile(file.GetStream());
+                string url = await new FirebaseStorage("gs://classaidapp.appspot.com")
+                .Child("data")
+                .PutAsync(file.GetStream());
                 lbl.Text = url;
             }
+
+            //var result = await FilePicker.PickAsync();
+            //if (result != null)
+            //{
+            //    Text = $"File Name: {result.FileName}";
+            //    if (result.FileName.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+            //        result.FileName.EndsWith("png", StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        var stream = await result.OpenReadAsync();
+            //        Image = ImageSource.FromStream(() => stream);
+            //    }
+            //}
         }
     }
 }
