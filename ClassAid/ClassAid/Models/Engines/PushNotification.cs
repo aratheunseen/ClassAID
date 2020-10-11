1f66a54002aa28f80e28bd3a72370be904ff7049
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-11 13:50:08 +0600

    Added push notification sender.

---
+++ ClassAid/ClassAid/Models/Engines/PushNotification.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Diagnostics;
+namespace ClassAid.Models.Engines
+{
+    public class PushNotification
+    {
+        public static void Send(string title, string message, string key)
+        {
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+
+            request.Headers.Add("authorization", "Basic ZDhmNDI4NWEtNWQ2My00OTQwLWJiOTMtNzU3NzU0MGI0ZmQ2");
+
+            byte[] byteArray = Encoding.UTF8.GetBytes("{"
+                                                    + "\"app_id\": \"7ab7ae00-d9e6-47cb-a4ed-f5045215fc9f\","
+                                                    + "\"contents\": {\"en\": \"" + message + "\"},"
+                                                    + "\"headings\": {\"en\": \"" + title + "\"},"
+                                                    + "\"filters\": [{\"field\": \"tag\", \"key\": \"AdminKey\"," +
+                                                    " \"relation\": \"=\", \"value\": \"" + key + "\"}]}");
+
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+
+            Debug.WriteLine(responseContent);
+        }
+    }
+}
