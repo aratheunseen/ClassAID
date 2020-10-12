8977438392eb03cc94ac325b1764706829a50ccb
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-12 19:06:42 +0600

    bluh bluh blu

--- ClassAid/ClassAid/Views/StudentProfile.xaml.cs
+++ ClassAid/ClassAid/Views/StudentProfile.xaml.cs
@@ -50,11 +50,11 @@ namespace ClassAid.Views
         }
         private async void AllocateRequestList(string key)
         {
-            //var data = await FirebaseHandler.GetPendingStudents(key);
-            //if (data != null)
-            //    RequestCollectionView.ItemsSource = data;
-            //else
-            //{
+            var data = await FirebaseHandler.GetPendingStudents(key);
+            if (data != null)
+                RequestCollectionView.ItemsSource = data;
+            else
+            {
                 List<Student> sample = new List<Student>()
                 {
                     new Admin() { Name = "Hasina", ID = "192311001", Phone = "0123567890" },
@@ -86,7 +86,7 @@ namespace ClassAid.Views
                     new Admin() { Name = "Khaleda", ID = "19231104", Phone = "0123567893" }
                 };
                 RequestCollectionView.ItemsSource = sample;
-            //}
+            }
         }
         private void BindData()
         {
