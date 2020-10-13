c33c508b26b9795d641f378545531e7d70013972
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-13 22:15:53 +0600

    added incoming and outgoinc chat datatemplate selector

---
+++ ClassAid/ClassAid/Views/Chat/ChatTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using Android.Preferences;
+using ClassAid.Models;
+using ClassAid.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ClassAid.Views.Chat
+{
+    class ChatTemplateSelector : DataTemplateSelector
+    {
+        DataTemplate incomingDataTemplate;
+        DataTemplate outgoingDataTemplate;
+        public ChatTemplateSelector()
+        {
+            incomingDataTemplate = new DataTemplate(typeof(IncomingViewCell));
+            outgoingDataTemplate = new DataTemplate(typeof(OutgoingViewCell));
+        }
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var message = item as ChatModel;
+            if (message == null)
+                return null;
+            return message.SenderKey == Preferences.Get(PrefKeys.Key, "")
+                ? outgoingDataTemplate
+                : incomingDataTemplate;
+        }
+    }
+}
