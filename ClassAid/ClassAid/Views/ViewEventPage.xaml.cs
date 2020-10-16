599903bebf791a3e9fa44899bb1e547b29ea534a
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-16 22:37:09 +0600

    Create event btn's position changed

--- ClassAid/ClassAid/Views/ViewEventPage.xaml.cs
+++ ClassAid/ClassAid/Views/ViewEventPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿
 using ClassAid.DataContex;
+using ClassAid.Models;
 using ClassAid.Models.Schedule;
 using ClassAid.Models.Users;
+using ClassAid.Views.AdminViews.Settings;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,7 +19,15 @@ namespace ClassAid.Views
         {
             InitializeComponent();
             eventListView.ItemsSource = App.Admin.EventList;
-            
+            if (Preferences.Get(PrefKeys.IsAdmin, false))
+                addScheduleBtn.Clicked += AddScheduleBtn_Clicked;
+            else
+                mainGrid.Children.Remove(addScheduleBtn);
+        }
+
+        private async void AddScheduleBtn_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AddEventPage());
         }
 
         private void OnCollectionViewScrolled(object sender, ItemsViewScrolledEventArgs e)
