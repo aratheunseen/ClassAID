0d6d7f1b05344ce99c7292d11c2dd52994657a1c
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-19 05:55:45 +0600

    Added more properties to student dashboard.

--- ClassAid/ClassAid/Views/StudentViews/Profile.xaml.cs
+++ ClassAid/ClassAid/Views/StudentViews/Profile.xaml.cs
@@ -1,11 +1,6 @@
 ﻿using ClassAid.DataContex;
 using ClassAid.Models.Schedule;
 using ClassAid.Models.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
