03e0246dd367c05a61f4a57060e7ec3905af93f3
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-10-18 15:40:42 +0600

    Motamuti works

---
+++ ClassAid/ClassAid/Views/StudentViews/Profile.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ClassAid.DataContex;
+using ClassAid.Models.Schedule;
+using ClassAid.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClassAid.Views.StudentViews
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profile : ContentPage
+    {
+        public Student Student { get; set; }
+        public Profile()
+        {
+            Student = LocalDbContex.GetStudentAsUser();
+            BatchDetails batch = LocalDbContex.GetBatchDetails();
+            //List<Student> students = LocalDbContex.GetStudents();
+            //List<Teacher> teachers = LocalDbContex.GetTeachers();
+            InitializeComponent();
+            logoutBtn.Command = new Command(() => App.LogOut());
+            userName.Text = Student.Name;
+            userID.Text = Student.ID;
+            userPhone.Text = Student.Phone;
+            userSection.Text = batch.Section;
+            userSemester.Text = batch.Semester;
+            userUniversity.Text = batch.University;
+            userDepartment.Text = batch.Department;
+        }
+    }
+}
